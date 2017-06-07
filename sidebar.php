40a0105c20d3ab6980d7ccb23cef4cd3d80fd31c
hiyelang <ilaozheng@outlook.com>
2017-06-07 19:09:42 +0800

    0.0.1 beta publish

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-	<div id="sidebar" class="w3-sidebar w3-light-grey w3-bar-block w3-card w3-animate-left" style="width:15%;">
+	<div class="w3-sidebar w3-light-grey w3-bar-block w3-card w3-animate-left" style="width:15%;">
 	  <div class="w3-bar">
 	  	<span v-on:click="hide" class="w3-bar-item w3-button w3-large">Chose &times;</span>
 	  </div>
