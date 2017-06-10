24ed5db9179790279d8c5308b4bb2b6ca2794714
hiyelang <ilaozheng@outlook.com>
2017-06-10 19:53:42 +0800

    change vue.js to vue.min.js

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="<?php $this->options->themeUrl('/css/w3.css'); ?>">
 	<link rel="stylesheet" href="<?php $this->options->themeUrl('/css/lyanz.css'); ?>">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<script src="<?php $this->options->themeUrl('/js/vue.js'); ?>"></script>
+	<script src="<?php $this->options->themeUrl('/js/vue.min.js'); ?>"></script>
 	<title><?php $this->options->title(); ?><?php $this->archiveTitle(); ?></title>
 </head>
 <body class="w3-container">
