993f65d554f9a67bbfb7a909123b0b748dd0e27b
hiyelang <ilaozheng@outlook.com>
2017-06-11 10:00:55 +0800

    update 201706111000 for add search form

--- sidebar.php
+++ sidebar.php
@@ -7,4 +7,10 @@
                ->parse('<a href="{permalink}" class="w3-bar-item w3-button">{title}</a>'); ?>
       <!--请注释或/注释下面的PHP语句以禁用/启用友情链接-->
 	  <?php $this->need('links.php'); ?>
+
+		<form method="post" action="" class="w3-container">
+		<input type="text" name="s" class="w3-input w3-border w3-light-grey w3-left w3-threequarter" type="text" value="输入关键字..">
+		<button type="submit" class="w3-button w3-right w3-blue-grey w3-quarter"><span class="fa fa-search"></span></button>
+		</form>
+	  </div>
 	</div>
