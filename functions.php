2355950bc90e5e3fb56e1ce027d0bfb6385e835f
hiyelang <ilaozheng@outlook.com>
2017-06-12 07:46:38 +0800

    update 201706120746 for add functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+function themeConfig($form){
+	#自定义顶部文字
+	$headerSay = new Typecho_Widget_Helper_Form_Element_Text('headerSay',NULL,NULL,_t("顶部文字"),_t("在这里填入的文字会出现在网页标题下"));
+	$form->addInput($headerSay);
+
+	#自定义统计代码
+	$accessCode = new Typecho_Widget_Helper_Form_Element_Textarea('accessCode',NULL,NULL,_t("统计代码"),_t("在这里填入统计代码"));
+	$form->addInput($accessCode);
+
+    #自定义侧边栏设置
+    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
+    array('sidebarShow' => _t('显示侧边栏'),
+    'sidebarLinks' => _t('显示友情链接')),
+    array('sidebarShow', 'sidebarLinks'), _t('侧边栏设置'));
+    
+    $form->addInput($sidebarBlock->multiMode());
+}
+
+?>
